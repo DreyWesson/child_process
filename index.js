f2703cf47fe4e2b25858ec3a366c968462716c2b
dreyWesson <dreyWesson@gmail.com>
2020-06-07 14:46:21 +0100

    trying out task with nodejs

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const prog = {
   node: "node",
 };
 
+handlePHP();
+handleJS();
+handleCPP();
+handlePY();
+
 // List the scripts
 // const child_pro = runner.spawn(prog.list, { cwd: "./node/child_process" });
 // child_pro.stdout.on("data", (data) => {
@@ -13,53 +18,65 @@ const prog = {
 // });
 
 // run php scripts
-var phpScriptPath = "./scripts/php.php";
-var argsString = "value1,value2,value3";
-runner.exec("php " + phpScriptPath + " " + argsString, function (
-  err,
-  phpResponse,
-  stderr
-) {
-  if (err) console.log(err);
-  console.log(phpResponse);
-});
+function handlePHP() {
+  var phpScriptPath = "./scripts/php.php";
+  var argsString = "value1,value2,value3";
+  runner.exec("php " + phpScriptPath + " " + argsString, function (
+    err,
+    phpResponse,
+    stderr
+  ) {
+    if (err) console.log(err);
+    console.log(phpResponse);
+  });
+}
 
 // run javascripts
-const cp = runner.spawn(prog.node, ["./scripts/app.js"]);
-cp.stdout.on("data", (data) => console.log(`${data}`));
+function handleJS() {
+  const cp = runner.spawn(prog.node, ["./scripts/app.js"]);
+  cp.stdout.on("data", (data) => console.log(`${data}`));
+}
 
 //run c++ script
-const compiler = "g++",
-  version = "-std=c++11",
-  out = "-o",
-  infile = "./scripts/hello.cpp",
-  outfile = "./scripts/hello.out",
-  name = "C++";
+function handleCPP() {
+  const compiler = "g++",
+    version = "-std=c++11",
+    out = "-o",
+    infile = "./scripts/hello.cpp",
+    outfile = "./scripts/hello.out",
+    name = "C++";
 
-const child = runner.execFile(
-  compiler,
-  [version, out, outfile, infile],
-  (error, out, err) => {
-    if (error) throw error;
-    else {
-      const executable = `./${outfile}`;
-      const execute = runner.execFile(executable, [name], (error, out, err) => {
-        if (error) throw error;
-        else console.log(out);
-      });
+  const child = runner.execFile(
+    compiler,
+    [version, out, outfile, infile],
+    (error, out, err) => {
+      if (error) throw error;
+      else {
+        const executable = `./${outfile}`;
+        const execute = runner.execFile(
+          executable,
+          [name],
+          (error, out, err) => {
+            if (error) throw error;
+            else console.log(out);
+          }
+        );
+      }
     }
-  }
-);
+  );
+}
 
 //Run python script
-function runScript() {
-  return runner.spawn("python", [
-    "-u",
-    path.join(__dirname, "./scripts/s.py"),
-    "--foo",
-    "some value for foo",
-  ]);
+function handlePY() {
+  function runScript() {
+    return runner.spawn("python", [
+      "-u",
+      path.join(__dirname, "./scripts/s.py"),
+      "--foo",
+      "some value for foo",
+    ]);
+  }
+  const subprocess = runScript();
+  // print output of script
+  subprocess.stdout.on("data", (data) => console.log(`data:${data}`));
 }
-const subprocess = runScript();
-// print output of script
-subprocess.stdout.on("data", (data) => console.log(`data:${data}`));
